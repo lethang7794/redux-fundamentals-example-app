ef6236172e7a44aa2c2eb3e7534a8319bf54bb2b
Thang Le Quang <lethang7794@gmail.com>
2021-01-19 18:50:39 +0700

    Use useSelector to access todos state from store

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import TodoListItem from './TodoListItem'
 
 const TodoList = () => {
+  const todos = useSelector(state => state.todos)
+
   return (
     <ul className="todo-list">
+      {todos.map((todo) => (
+        <TodoListItem key={todo.id} {...todo} />
+      ))}
     </ul>
   )
 }
